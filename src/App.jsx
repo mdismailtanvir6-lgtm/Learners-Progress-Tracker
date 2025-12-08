da5eecdd8f90ad5916d1dd1af6c23e22e0c3e343
Ismail Hossain <mdismailtanvir6@gmail.com>
2025-12-08 09:19:17 +0600

    this is react demo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import React from "react";
+
+const App = () => {
+  return <div className="text-[14px] leading-[23px]">hello</div>;
+};
+
+export default App;
