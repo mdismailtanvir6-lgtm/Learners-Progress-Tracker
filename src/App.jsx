c27bef965e3ee4ba71748bdd5612e8f0112991fa
Ismail Hossain <mdismailtanvir6@gmail.com>
2026-01-11 21:16:34 +0600

    firebase set up done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
-
 import React from "react";
+import { auth } from "./firebase/config";
 
 const App = () => {
-  return <div className="text-[14px] leading-[23px]">hello</div>;
+  console.log("Firebase Auth:", auth);
+
+  return <div className="text-[14px] leading-[23px]">Firebase connected</div>;
 };
 
 export default App;
