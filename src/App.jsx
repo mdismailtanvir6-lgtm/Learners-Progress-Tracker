1f5ed803f2e43352b7575dade5bc39fc9c835bf1
Ismail Hossain <mdismailtanvir6@gmail.com>
2026-01-14 17:41:34 +0600

    step 4 : Protected routes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
 import "./App.css";
-// import React from "react";
-import { auth } from "./firebase/config";
-import { observeAuthState } from "./services/authService";
-import { useEffect } from "react";
-import { useAuth } from "./context/AuthContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Dashboard from "./pages/dashboard/DashboardHome";
+import ProtectedRoute from "./routes/ProtectedRoute";
+
 
 const App = () => {
-  // ========= firebase auth =======
-  console.log("Firebase Auth:", auth);
-
-  // ========= user observer auth state =======
-  useEffect(() => {
-    const unsubscribe = observeAuthState((user) => {
-      console.log("Auth State Changed:", user);
-    });
-
-    return () => unsubscribe();
-  }, []);
-
-  const { user, loading } = useAuth();
-
-  if (loading) return <p>Checking authentication...</p>;
-
   return (
-    <div>
-      {user ? (
-        <h2>Welcome, {user.email}</h2>
-      ) : (
-        <h2>User not logged in or email not verified</h2>
-      )}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/pages/auth/Login.jsx
@@ -0,0 +1,6 @@
+const Login = () => {
+  return <h2>Login Page (UI later)</h2>;
+};
+
+export default Login;
+
